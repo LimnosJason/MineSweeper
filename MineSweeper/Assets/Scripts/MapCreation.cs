815aee58ecaf7a823f38e485684df290e5efd4bd
LimnosJason <limnosjason@gmail.com>
2023-03-23 14:00:22 +0200

    Map Creation 5x5

---
+++ MineSweeper/Assets/Scripts/MapCreation.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreation : MonoBehaviour
+{
+    [SerializeField] GameObject cornerRoomPrefab;
+    [SerializeField] GameObject parallelRoomPrefab;
+    [SerializeField] GameObject mapGameObject;
+
+    private Vector3 savedPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlaceRoomsMethod();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlaceRoomsMethod(){
+        int i,j,row=50,col=50;
+        savedPosition=mapGameObject.transform.position;
+        //Place rooms
+        for(i=0;i<row;i++){
+            savedPosition= savedPosition + new Vector3(-10.0f, 0.0f, col*10.0f);
+            for(j=0;j<col;j++){
+                savedPosition= savedPosition + new Vector3(0.0f, 0.0f, -10.0f);
+                if(i==0&&j==0){
+                    GameObject instantiatedObject=Instantiate(cornerRoomPrefab);
+                    instantiatedObject.name = "Corner Room " + (i+1).ToString() + " " + (j+1).ToString();
+                    instantiatedObject.transform.SetParent(mapGameObject.transform);
+                    instantiatedObject.transform.position = savedPosition;
+                }
+                else if(j==col-1){
+                    GameObject instantiatedObject=Instantiate(parallelRoomPrefab);
+                    instantiatedObject.name = "Border Room " + (i+1).ToString() + " " + (j+1).ToString();
+                    instantiatedObject.transform.SetParent(mapGameObject.transform);
+                    instantiatedObject.transform.position = savedPosition;
+                }
+                else{
+                    GameObject instantiatedObject=Instantiate(cornerRoomPrefab);
+                    instantiatedObject.name = "Corner Room " + (i+1).ToString() + " " + (j+1).ToString();
+                    instantiatedObject.transform.SetParent(mapGameObject.transform);
+                    instantiatedObject.transform.position = savedPosition;
+                }
+            }
+        }
+    }
+}
