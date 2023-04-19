df3af3a57308d0be41f26e3581e087f6c203cdeb
LimnosJason <limnosjason@gmail.com>
2023-04-20 00:39:51 +0300

    Lose Canvas

--- MineSweeper/Assets/Scripts/PlayerEndResultScript.cs
+++ MineSweeper/Assets/Scripts/PlayerEndResultScript.cs
@@ -56,6 +56,7 @@ public class PlayerEndResultScript : MonoBehaviour
     }
 
     public void PlayerDeath(){
+        Cursor.lockState=CursorLockMode.None;
         settingsScript.FreezeAndRemoveAim();
         StartCoroutine(LoseCoroutine());
     }
